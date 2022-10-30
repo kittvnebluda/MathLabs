10cf77325d6515cd1dc892f9780ed7134f75b941
KiBots <team174sz@gmal.com>
2022-10-30 23:33:17 +0300

    Lab 3 is not done

---
+++ linalg/lab3/sasha.py
@@ -0,0 +1,74 @@
+def det(mat):
+    return mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0]
+
+
+def trans(mat):
+    mat_tr = []
+    for i in range(len(mat[0])):
+        mat_tr.append([])
+        for j in range(len(mat)):
+            mat_tr[i].append(mat[j][i])
+    return mat_tr
+
+
+def solve(mat, ans):
+    detA = det(mat)
+    mat = trans(mat)
+    det1 = det([ans, mat[1]])
+    det2 = det([mat[0], ans])
+    return [det1 / detA, det2 / detA]
+
+
+def check_mn(f, s):
+    return 0 if s == 0 else f / s
+
+
+def is_line(a, b):
+    mn = a[0][0] / a[1][0] \
+        if check_mn(a[0][0], a[1][0]) else a[0][1] / a[1][1] \
+        if check_mn(a[0][1], a[1][1]) else b[0] / b[1] \
+        if check_mn(b[0], b[1]) else 0
+
+    return (a[0][0] * mn == a[1][0] and a[0][1] * mn == a[1][1] and b[0] * mn == b[1]) or \
+           (a[0][0] == a[1][0] * mn and a[0][1] == a[1][1] * mn and b[0] == b[1] * mn)
+
+
+with open("input.txt", "r") as inp:
+    arr = list(map(float, inp.readline().split()))
+
+a = [arr[:2], arr[2:4]]
+b = arr[4:]
+
+
+def task():
+    if det(a) != 0:
+        return 2, solve(a, b)
+
+    if not any(arr):
+        return 5, []
+
+    if is_line(a, b) and not any([a[1][0], a[1][1], b[1]]):
+        if a[0][0] == 0 and a[0][1] != 0:
+            return 4, [b[0] / a[0][1]]
+        if a[0][1] == 0 and a[0][0] != 0:
+            return 3, [b[0] / a[0][0]]
+
+    if is_line(a, b):
+        if a[1][0] == 0 and a[1][1] != 0:
+            return 4, [b[1] / a[1][1]]
+        if a[1][1] == 0 and a[1][0] != 0:
+            return 3, [b[1] / a[1][0]]
+
+    if is_line(a, b):
+        if a[0][1] != 0:
+            return 1, [-1 * a[0][0] / a[0][1], b[0] / a[0][1]]
+        elif a[1][1] != 0:
+            return 1, [-1 * a[1][0] / a[1][1], b[1] / a[1][1]]
+
+    return 0, []
+
+
+ans = task()
+
+with open("output.txt", "w") as out:
+    out.write(str(ans[0]) + " " + str(ans[1]).replace('[', '').replace(']', '').replace(',', ''))
--- linalg/lab3/solution.py
+++ linalg/lab3/solution.py
@@ -1,39 +1,77 @@
+from copy import deepcopy
+
+
+class Matrix:
+    def __init__(self, arr):
+        """
+        Class for 2x2 matrices
+        """
+        self.matrix = arr
+        self.shape = len(arr), len(arr[0])
+
+    def replace_column(self, col, vec):
+        new_matrix = deepcopy(self.matrix)
+
+        new_matrix[0][col] = vec[0]
+        new_matrix[1][col] = vec[1]
+
+        return Matrix(new_matrix)
+
+    def getD(self):
+        return self.matrix[0][0] * self.matrix[1][1] - self.matrix[0][1] * self.matrix[1][0]
+
+    @staticmethod
+    def from_what(flat_matrix, rows, columns):
+        new_matrix = []
+        for i in range(rows):
+            row = []
+            for j in range(columns):
+                row.append(flat_matrix[columns * i + j])
+            new_matrix.append(row)
+
+        return Matrix(new_matrix)
+
+
 with open("input.txt") as f:
-    a, b, c, d, p, q = map(float, next(f).split())
+    slae = tuple(map(float, next(f).split()))
+    a, b, c, d, p, q = slae
+
+array, bias = slae[:4], slae[4:]
+m0 = Matrix.from_what(array, 2, 2)
+
+m1 = m0.replace_column(0, bias)
+m2 = m0.replace_column(1, bias)
+
+d0 = m0.getD()
+d1 = m1.getD()
+d2 = m2.getD()
 
 with open("output.txt", "w") as f:
-    try:
-        if d == 0 and c == 0 or a == 0 and b == 0:
-            f.write(f"5")
+    if d0 != 0:
+        f.write(f"2 {d1/d0} {d2/d0}")
 
-        elif d == 0:
-            x = q / c
-            f.write(f"3 {x}")
+    elif not any(slae):
+        f.write("5")
 
-        elif b == 0:
-            x = p / a
-            f.write(f"3 {x}")
+    elif d == 0 and c != 0:
+        x = q / c
+        f.write(f"3 {x}")
 
-        elif c == 0:
-            y = q / d
-            f.write(f"4 {y}")
+    elif a != 0 and b == 0:
+        x = p / a
+        f.write(f"3 {x}")
 
-        elif a == 0:
-            y = p / b
-            f.write(f"4 {y}")
+    elif c == 0 and d != 0:
+        y = q / d
+        f.write(f"4 {y}")
 
-        elif a / c == b / d == p / q:
-            f.write(f"1 {-a / b} {p / b}")
+    elif a == 0 and b != 0:
+        y = p / b
+        f.write(f"4 {y}")
 
-        else:
-            y = (c * p - a * q) / (c * b - a * d)
-            x = (q - d * y) / c
+    elif a / c == b / d == p / q:
+        f.write(f"1 {-a / b} {p / b}")
 
-            f.write(f"2 {x} {y}")
-
-    except Exception as e:
-        print(e)
+    else:
         f.write("0")
-
-
 
